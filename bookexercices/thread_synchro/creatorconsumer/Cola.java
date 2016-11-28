bac90f9fd5e3d0e26d6e678bb201ae8fcfbfa9ac
AEmbid <adrian.embid@gmail.com>
2016-11-28 10:35:05 +0100

    Added syncho exercices

---
+++ bookexercices/thread_synchro/creatorconsumer/Cola.java
@@ -0,0 +1,24 @@
+package creatorconsumer;
+
+public class Cola {
+	private int numero;
+	private boolean disponible = false;
+	
+	public synchronized int get() {
+		while (disponible == false) {
+			try {wait();} catch (InterruptedException e) {}
+		}
+		disponible = false;
+		notifyAll();
+		return numero;
+	}
+	
+	public synchronized void put(int valor) {
+		while (disponible == true) {
+			try {wait();} catch (InterruptedException e) {}
+		}
+		numero = valor;
+		disponible = true;
+		notifyAll();
+	}
+}
